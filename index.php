3f1d64f9fed11b043b26ad60fc2c2dbffd7bc306
Darcrazywill <darinakokoreva763@gmail.com>
2025-11-06 11:08:43 +0300

    2 Вставили код

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Мой первый PHP-сайт</title>
+</head>
+<body>
+
+    <h1>Приветствие</h1>
+    <?php
+        // Это простой комментарий в PHP
+        $greeting = "Привет, мир!";
+        echo "<p>" . $greeting . "</p>";
+
+        // Пример вывода текущей даты
+        echo "<p>Сегодня: " . date("d.m.Y") . "</p>";
+    ?>
+
+    <p>Это обычный HTML.</p>
+
+</body>
+</html>
